8d21f1690b3edc997f6e3a1aa8895b1da8defb98
100mensoul <momo2ro1260@gmail.com>
2025-05-15 17:25:56 +0900

    videos.js を作成

---
+++ video/videos.js
@@ -0,0 +1,35 @@
+const videoData = [
+  {
+    seriesTitle: "へーんしん！ULTLAシリーズ",
+    seriesId: "ultla",
+    videos: [
+      { id: "XXXXXXXXXXX", title: "ULTLA変身編①" },
+      { id: "YYYYYYYYYYY", title: "ULTLA変身編②" },
+      { id: "ZZZZZZZZZZZ", title: "ULTLA変身編③" },
+    ]
+  },
+  {
+    seriesTitle: "地域へ飛び出せ！STEAM CHAOS",
+    seriesId: "steamchaos",
+    videos: [
+      { id: "AAAABBBBCCC", title: "STEAM大作戦①" },
+      { id: "DDDDEEEEFFF", title: "STEAM大作戦②" },
+    ]
+  },
+  {
+    seriesTitle: "眠れる獅子たちよ、これがGODHANDSだ！",
+    seriesId: "godhands",
+    videos: [
+      { id: "GGGGHHHHIII", title: "GODHANDS Episode 1" },
+      { id: "JJJJKKKKLLL", title: "GODHANDS Episode 2" },
+    ]
+  },
+  {
+    seriesTitle: "すべての学びはここから〜自分学のススメ〜",
+    seriesId: "jibungaku",
+    videos: [
+      { id: "MMMMNNNNOOO", title: "自分学Intro" },
+      { id: "PPPQQQRRRSS", title: "自分学実践編" },
+    ]
+  }
+];
